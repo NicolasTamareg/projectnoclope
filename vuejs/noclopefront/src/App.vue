154a0fc21a9409199d90781ecd20b5589a081e76
Hilel Hamidi <hilel.hamidi@gmail.com>
2022-12-07 22:47:12 +0100

    formulaire ok

--- vuejs/noclopefront/src/App.vue
+++ vuejs/noclopefront/src/App.vue
@@ -1,13 +1,20 @@
 <script setup>
 import { RouterView } from "vue-router";
 import NavBar from "./components/NavBar.vue";
+
 </script>
 
 <template>
   <header>
     <NavBar />
   </header>
+  
+ 
   <RouterView />
+  <footer>
+        Noclope &copy;Tout droits reservés
+    </footer>
 </template>
 
-<style scoped></style>
+<style scoped>
+</style>
