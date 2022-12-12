cb9bb25fb9e83a5a1e0bd88fa30b53f0ed1805e9
nabil <amr.nabz@gmail.com>
2022-12-12 10:51:49 +0100

    tailwind enlevé le temps de regler les formulaires

--- vuejs/noclopefront/src/views/LoginView.vue
+++ vuejs/noclopefront/src/views/LoginView.vue
@@ -47,18 +47,17 @@ export default {
     },
   },
   computed:{
-    // passwordValid: function () {
-    //   if (this.password.length === 0) return "";
-    //   const isLengthOk = this.password.length =< 8;
-    //   if (!isLengthOk) return "border-red";
-
-    //   for (let char of this.password) {
-    //     if (this.password == 5) {
-    //       return "border-red";
-    //     }
-    //   }
-    //   return "border-green";
-    // },
+    passwordValid: function () {
+     if (this.password.length === 0) return "";
+      const isLengthOk = this.password.length <= 8;
+      if (!isLengthOk) return "border-red";
+       for (let char of this.password) {
+         if (this.password == 5) {
+          return "border-red";
+        }
+      }
+      return "border-green";
+    },
 
 
   },
@@ -75,7 +74,7 @@ export default {
           name="email"
           id="emailInput"
           v-model="email"
-          placeholder="test@test.com"
+          placeholder="email"
           required
         />
         
@@ -88,19 +87,19 @@ export default {
           placeholder="********"
           required
         />
-      <!-- <RouterLink to="/profil">
+       <RouterLink to="/profil">
         <p v-if="result === true"></p>
         <p v-else-if="result === false" class="error">
           Adresse mail ou mot de passe est invalide.
         </p>
-      </RouterLink> -->
+      </RouterLink> 
       <input class="login-button" type="submit" value="Se connecter" />
     </form>
 
     <p v-if="feedbackMessage">{{feedbackMessage}}</p>
   </div>
  
-<div class="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+<!-- <div class="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
   <div class="w-full max-w-md space-y-8">
     <div>
       <img class="mx-auto h-12 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="Your Company">
@@ -110,7 +109,7 @@ export default {
         <a href="#" class="font-medium text-indigo-600 hover:text-indigo-500">commence a te connecter</a>
       </p>
     </div>
-    <form class="mt-8 space-y-6" action="#" method="POST">
+    <form class="mt-8 space-y-6" action="#" @submit.prevent="login" method="POST">
       <input type="hidden" name="remember" value="true">
       <div class="-space-y-px rounded-md shadow-sm">
         <div>
@@ -137,17 +136,17 @@ export default {
       <div>
         <button type="submit" value="Se connecter" class="group relative flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
           <span class="absolute inset-y-0 left-0 flex items-center pl-3">
-            <!-- Heroicon name: mini/lock-closed -->
-            <svg class="h-5 w-5 text-indigo-500 group-hover:text-indigo-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-              <path fill-rule="evenodd" d="M10 1a4.5 4.5 0 00-4.5 4.5V9H5a2 2 0 00-2 2v6a2 2 0 002 2h10a2 2 0 002-2v-6a2 2 0 00-2-2h-.5V5.5A4.5 4.5 0 0010 1zm3 8V5.5a3 3 0 10-6 0V9h6z" clip-rule="evenodd" />
+            Heroicon name: mini/lock-closed
+            <svg class="h-5 w-5 text-indigo-500 group-hover:text-indigo-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true"> -->
+               <!-- <path fill-rule="evenodd" d="M10 1a4.5 4.5 0 00-4.5 4.5V9H5a2 2 0 00-2 2v6a2 2 0 002 2h10a2 2 0 002-2v-6a2 2 0 00-2-2h-.5V5.5A4.5 4.5 0 0010 1zm3 8V5.5a3 3 0 10-6 0V9h6z" clip-rule="evenodd" />
             </svg>
           </span>
           Se connecter
         </button>
       </div>
-    </form>
+    </form> 
   </div>
-</div>
+</div>  -->
 
 </template>
 
@@ -164,4 +163,4 @@ export default {
 .border-red {
   border-width: 2px;
 }
-</style>
+</style> 
