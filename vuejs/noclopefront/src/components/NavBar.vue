3561609183bdfc94aff1e08c07a917a41e2a0a9d
nabil <amr.nabz@gmail.com>
2022-12-13 16:27:48 +0100

    fix

--- vuejs/noclopefront/src/components/NavBar.vue
+++ vuejs/noclopefront/src/components/NavBar.vue
@@ -45,7 +45,7 @@ export default {
             <div class="ml-10 flex items-baseline space-x-4">
               <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
               <h1 class="text-gray-300 hover:text-white px-3 py-2 rounded-md text-lg font-bold font-large">NO CLOPE</h1>
-              <a href="#" class="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium" aria-current="page"><RouterLink to="/home">Acceuil</RouterLink></a>
+              <a href="#" class="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium" aria-current="page"><RouterLink to="/home">Accueil</RouterLink></a>
 
               <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"><RouterLink to="/register">Créer un compte</RouterLink></a>
 
