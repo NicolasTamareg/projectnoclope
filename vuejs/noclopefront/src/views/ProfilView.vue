1f44dd67339cea1d395a7c3fe6c4a1c4801fb20c
nabil <amr.nabz@gmail.com>
2022-12-16 12:10:42 +0100

    css toujours csss

--- vuejs/noclopefront/src/views/ProfilView.vue
+++ vuejs/noclopefront/src/views/ProfilView.vue
@@ -44,78 +44,124 @@ export default {
 </script>
 
 <template>
-
-  <div>
-    <h2>Bonjour</h2>
-    
-        <p>Nom: {{user.firstname}}</p>
-        <p>Prénom: {{user.lastname}}</p>
-      
+  <div class="all">
+    <div class="user-title" >
+    <h2>Bonjour {{user.firstname}} {{user.lastname}}</h2>
   </div>
 
   <div class="card">
-  
-
-
-    <div class="objectif">
-      <h1>Jour 1</h1>
+      <div class="objectif">
+        <h1>Jour 1</h1>
+      <div></div>  
       <div class="voyage">
-<h3>Voyage Voyage</h3>
+        <h3>Voyage</h3>
       </div>
-      <div class="separe"></div>
-      <h2>100/1000€</h2>
+      
+      <h4>100/1000€</h4>
       <div class="progression">
       </div>
     </div>
-    <a href="/craquer"><input type="submit" value="J'ai craqué"></a>
-    <a href="/ange"><input type="submit" value="Contacter mes anges gardiens"></a>
-    <a href="/avance"><input type="submit" value="Demander une avance"></a>
+    <div class="links">
+      <a href="/craquer"><input type="submit" value="J'ai craqué"></a>
+      <a href="/ange"><input type="submit" value="Contacter mes anges gardiens"></a>
+      <a href="/avance"><input type="submit" value="Demander une avance"></a>
+    </div>
   </div>
+  </div>
+
+ 
 </template>
 
 <style scoped>
+.all{
+  background: linear-gradient(to bottom, #bdc3c7, #2c3e50);
 
+}
+.card{
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
 .objectif{
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  text-align: center;
   border: solid 1px black;
   height: 500px;
-  width: 400px;
-  background-color: grey;
+  width: 600px;
+  background-color: rgb(31 41 55 );
   margin-top: 0.25rem;
-  padding: 6px;
+  padding: 10px;
   border-radius: 8px;
   border: 1px solid black;
-  box-shadow: 2px 2px 10px 2px lightgray;
+  
 }
 .voyage{
-  margin-left: 30%;
-  margin-top: 20%;
-  height: 150px;
-  width: 150px;
-  background-color: black;
-  color: beige;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 300px;
+  width: 400px;
+  background-image: url(../views/img/projet.jpg);
+  color: black;
+  font-weight: bold;
+  background-position: center;
+  background-size: cover;
   margin-top: 0.25rem;
   padding: 6px;
   border-radius: 8px;
   border: 1px solid black;
-  box-shadow: 2px 2px 10px 2px lightgray;
+  align-self: center;
 }
 .progression{
   height: 20px;
   width: 90%;
-  background-color: aquamarine;
+  background-color: chartreuse;
   margin-left: 20px;
   margin-top: 0.25rem;
   padding: 6px;
   border-radius: 8px;
   border: 1px solid black;
-  box-shadow: 2px 2px 10px 2px lightgray;
+  
 }
-h2{
+.user-title{
+  display: flex;
   text-align: center;
+  align-content: center;
+  justify-content: center;
+  padding: 20px;
 }
 .separe{
   height: 100px;
 }
-
-
+h1{
+  padding: 10px;
+  color: white;
+}
+h2{
+  padding: 20px;
+  font-size: 2em;
+}
+h4{
+  padding: 20px;
+  color: white;
+}
+.links{
+  display: flex;
+    justify-content: space-between;
+    padding: 10px;
+    align-self: normal;
+}
+a{
+  font-size: 1.3em;
+  color: white;
+}
+a:hover{
+  background-color: red;
+  border-radius: 30px;
+  padding: 5px;
+}
 </style>
