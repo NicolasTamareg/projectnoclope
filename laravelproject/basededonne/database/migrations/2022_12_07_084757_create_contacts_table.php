763801eee9d38205052b14346153528eea3785a9
nicolas.tamareg <ntamareg13@gmail.com>
2022-12-19 15:06:09 +0100

    mail cracking

--- laravelproject/basededonne/database/migrations/2022_12_07_084757_create_contacts_table.php
+++ laravelproject/basededonne/database/migrations/2022_12_07_084757_create_contacts_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
         Schema::create('contacts', function (Blueprint $table) {
         $table->id();
         $table->string('email');
+        $table->integer('verified')->nullable();
         $table->string('firstname');
         $table->string('lastname');
         $table->unsignedBigInteger('user_id');
